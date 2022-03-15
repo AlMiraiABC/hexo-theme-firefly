492370cc04f569087cd1dcd2abe2e6a604b4405e
AlMirai <live.almirai@outlook.com>
2022-03-15 13:24:30 +0800

    feat: ✨ change welcome info
    Feat:
    1. change welcome info(welcome.js)

--- scripts/events/welcome.js
+++ scripts/events/welcome.js
@@ -1,17 +1,7 @@
 const logger = require('hexo-log')()
 
 hexo.on('ready', () => {
-  const { version } = require('../../package.json')
-  logger.info(`
-  ===================================================================
-                                                                     
-      #####  #    # ##### ##### ###### #####  ###### #      #   #    
-      #    # #    #   #     #   #      #    # #      #       # #     
-      #####  #    #   #     #   #####  #    # #####  #        #     
-      #    # #    #   #     #   #      #####  #      #        #      
-      #    # #    #   #     #   #      #   #  #      #        #    
-      #####   ####    #     #   ###### #    # #      ######   #  
-
-                            ${version}
-  ===================================================================`)
+    const { name, version, bugs, homepage } = require('../../package.json')
+    logger.info(`${name}@${version}, see more at ${homepage}.
+      If you have any problems, please visit ${bugs.url}.`)
 })
